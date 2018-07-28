3aa14a1840a0d474731dbaca6b8f64d286337f7c
Rafael Peralta Jr <rperaltajr@gmail.com>
2018-07-28 02:34:55 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Python Flask From Scratch
+
+#### Setup Flask [http://flask.pocoo.org/]
+`pip install flask`
+
+#### Run server
+`python app.py`
---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id':           1,
+            'title':        'Article One',
+            'body':         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores facere odio nobis, excepturi quod molestias quaerat explicabo, sequi voluptatum animi quos vel consequuntur cum culpa iste molestiae ab corrupti. Soluta!',
+            'author':       'Rafael Peralta Jr',
+            'create_date':  '07-27-2018'
+        },
+        {
+            'id':           2,
+            'title':        'Article Two',
+            'body':         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores facere odio nobis, excepturi quod molestias quaerat explicabo, sequi voluptatum animi quos vel consequuntur cum culpa iste molestiae ab corrupti. Soluta!',
+            'author':       'John Doe',
+            'create_date':  '04-25-2017'
+        },
+        {
+            'id':           3,
+            'title':        'Article Three',
+            'body':         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores facere odio nobis, excepturi quod molestias quaerat explicabo, sequi voluptatum animi quos vel consequuntur cum culpa iste molestiae ab corrupti. Soluta!',
+            'author':       'Brad Traversy',
+            'create_date':  '04-25-2018'
+        }
+    ]
+    return articles # return articles so that you can import it later
